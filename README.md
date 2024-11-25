6c5dcd4c4ce29e1c40974c90fa36ab228fc866c5
Thomas Wilkerling <thomas@nextapps.de>
2024-11-25 09:45:10 +0100

    Snowflake v2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Javascript Snowflakes (HTML5 Canvas)
+# Web Snowflake Plugin (Canvas-based, 2Kb)
 ### Usage
 
 Insert this snippet into `<HEAD>` or `<BODY>` tag of your html page:
@@ -9,7 +9,182 @@ Insert this snippet into `<HEAD>` or `<BODY>` tag of your html page:
 
 ### Demo
 
-[https://nextapps.de/snowflake/](https://nextapps.de/snowflake/)
+[https://nextapps-de.github.io/snowflake/demo/index.html](https://nextapps-de.github.io/snowflake/demo/index.html)
+
+### Custom Configuration
+
+Apply a config object to `window.SnowflakeConfig` __before__ loading the library:
+
+```js
+window.SnowflakeConfig = {
+    speed: 1.2,
+    size: 1.2,
+    quality: 2.0,
+    density: 2.0,
+    opacity: 0.5,
+    index: 9,
+    mount: document.body,
+    image: "path-to-file.png",
+    style: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+    }
+};
+```
+
+### Disable Autostart
+
+```js
+window.SnowflakeConfig = { 
+    start: false
+};
+```
+
+Or
+
+```js
+window.SnowflakeConfig = { 
+    stop: true
+};
+```
+
+## Controls
+
+### Hide/Stop Snowflake
+
+```js
+Snowflake.stop();
+```
+
+### Show/Start Snowflake
+
+```js
+Snowflake.start();
+```
+
+> The library will automatically start when loading the library
+
+### Set Flake Speed
+
+```js
+// twice
+Snowflake.speed(2);
+// half
+Snowflake.speed(0.5);
+// standard
+Snowflake.speed(1);
+```
+
+### Set Flake Density
+
+```js
+// twice
+Snowflake.density(2);
+// half
+Snowflake.density(0.5);
+// standard
+Snowflake.density(1);
+```
+
+> The total amount of flakes auto-scales accordingly to the available viewport.
+
+### Set Flake Size
+
+```js
+// twice
+Snowflake.size(2);
+// half
+Snowflake.size(0.5);
+// standard
+Snowflake.size(1);
+```
+
+### Set Flake Quality
+
+```js
+// Best
+Snowflake.quality(2);
+// Low
+Snowflake.quality(0.5);
+// Standard
+Snowflake.quality(1);
+```
+
+> Higher quality consumes more performance, lower quality will instead gain performance.
+
+### Custom Mount Canvas
+
+```js
+Snowflake.mount(document.body);
+```
+
+### Adjust Opacity:
+
+```js
+Snowflake.opacity(0.5);
+```
+
+### Adjust Z-Index:
+
+```js
+Snowflake.index(1);
+```
+
+> If you have issues to place the snowflake as background behind other elements try using a negative z-index value e.g. `Snowflake.index(-1);`.
+
+### Adjust Position
+
+```js
+Snowflake.style({ 
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+});
+```
+
+### Custom Flake Image-Source
+
+```js
+ // SVG is not supported
+Snowflake.image("path-to-file.png");
+```
+
+Or
+
+```js
+Snowflake.image("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABAAgMAAADXB5lNAAAADFBMVEXk7PTk7PTk7PQAAAAAkx47AAAABHRSTlOQSfwAyZGfwAAAAdRJREFUeF6Fk7FOhEAQhvcNrHwCO+xteQfj5hKfQHmAa6m0sCGxv5soiSxcQWd5lJprzcWExt7KUIzJBRh3djYIWEgCu/kIP//M/qNIrr7yGw++EI4m4LBdZxPQ3N6XE9AWRTUBFBQ0BbvNCLShFSl5FfCOQCT3uwPXv+DKgWjpAa5yB+o1ULOEknCbOoCPIE7xLnGgK4Cap/uEsIgdWATATkNCs7eA94l1aojQUgGZdWr1agGkL3Lr1OpFx9rXEsXUA3VmKK4GNobpADBjUCcD6HIGUShARBBEwoMaEDAdAcwQrIQAEUEQCVKv1LKn5Yq9V3SiPsOeXa+3/KzajTokLI+P3IsobjLVpvQZ2p7YXnQb2lVqb3R9oxeB2evzVAcLVcyuv2Bf6MsbrQOj9fkDf9JaJWuJRd1WHUqS3wJREDel2lVi7Jkby8Z21LOlMz7GpqJT9U1kLRmEyC5EH778FKGGcYMSBMxGIAoRunwAVsI1OR4ApgxERIkEAxFRIuGOMvdAa+MPW2ufD/bu4yCA3/4GhnQA1EmkfD4K4PIldLNYwhDcw3JVEnJPFPdiFu2refhfGPQOvI0GKOF1PGIpzYbQTAGHfwJ4PP4ZdYRyAtiHgB9fc1w25BtOVgAAAABJRU5ErkJggg==");
+```
+
+Disable flake image and just draw a basic circle:
+
+```js
+Snowflake.image(false);
+```
+
+### Custom CSS
+
+```css
+#snowflake{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    opacity: 1;
+    z-index: 99;
+}
+```
 
 ### License:
 
--- snowflake.js
+++ snowflake.js
@@ -3,89 +3,149 @@
     License: Apache License 2.0
 */
 
-(function(window, document) {
+(function(){
 
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
-    var style = canvas.style;
-    var width, height, count;
-    var snowflakes, snowflake;
+    "use strict";
 
-    style.position = "fixed";
-    style.top = "0";
-    style.left = "0";
-    style.width = "100%";
-    style.height = "100%";
-    style.zIndex = "999999";
-    style.pointerEvents = "none";
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const dpi = window.devicePixelRatio || 1;
+    const image = new Image();
+    const base64 = window["dRg4!"] = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAGFBMVEXk7PTk7PTk7PTk7PTk7PTk7PQAAADk7PQLtLMUAAAAB3RSTlO18FHbI4YAkU+M0QAAAVVJREFUeF7F1T1PwzAQgGGGouwg5B8QEWaioqyVSsNKoXL2KPYcE6L371PLcp1GjU7A0IztI53Pvo8bK3y/BnkpgGoQAFwXmPvCg/ZZL4A3hg94rPheBOFbBCaCiyHqo9gpAHc44m4Ots6L9h1uC/+/Ws/AE3qapuFrBpo5GCdgX14G+UMAHaxsRjEFLb3dQB0BB0U9BR1uRwS2Acdw/hYVCkYbQJcB4zlogL6OWbQV9OchMhiKlKYBxinYA9omsAW4SyAHWCfQKgagjGADVLgUogGdA6sAdh4bGCMw0PuwOO2BCQlmoBPQ8eAQUzIJdKo8JQunBHN3uodXa8NZAfCRbPg5glnN6eW+2BBy+RtIIQ4JSIcU0xQvSrxq6bHE5xYLRi45uWjlspcbR249uXnl9pcHiDyC0hD7hJc0xMQxKA9SeRTLw1xeB9ffWfJilVfz/7f/D7vzJWHzmXRTAAAAAElFTkSuQmCC";
+
+    let width, height, count, raf = 0, last = 0;
+    let speed = 1, size = 3, density = 1, quality = 1.5, autostart = true;
+
+    const snowflakes = [];
+    const buffer = [];
+    const flakes = [
+        { r: 1.5, o: 0.3 },
+        { r: 1.7, o: 0.4 },
+        { r: 1.9, o: 0.6 },
+        { r: 2.1, o: 0.8 },
+        { r: 2.3, o: 1.0 },
+        { r: 2.5, o: 1.0 },
+        { r: 2.7, o: 0.8 },
+        { r: 2.9, o: 0.6 },
+        { r: 3.1, o: 0.4 },
+        { r: 3.3, o: 0.3 }
+    ];
+
+    function createBuffer(index){
+
+        const flake = flakes[index];
+        const canvas = buffer[index] = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        const width = Math.ceil(flake.r * dpi * quality * size);
+        const blur = flake.r < 2.3
+            ? (2.3 - flake.r) * 0.9 * dpi * quality
+            : flake.r > 2.3
+                ? (flake.r - 2.3) * 1.1 * dpi * quality
+                : 0;
+
+        canvas.height = canvas.width = width;
+        ctx.globalAlpha = flake.o;
+        ctx.filter = ctx.webkitFilter = "blur(" + blur + "px)";
+        ctx.drawImage(
+            image,
+            0,
+            0,
+            image.width,
+            image.height,
+            0,
+            0,
+            width,
+            width
+        );
+
+        ctx.globalAlpha = flake.o / 1.5;
+        ctx.fillStyle = "#e4ecf4";
+        ctx.beginPath();
+        ctx.arc(
+            width / 2,
+            width / 2,
+            width / 2 / 2,
+            0,
+            Math.PI * 2,
+            false
+        );
+        ctx.closePath();
+        ctx.fill();
+
+        return canvas;
+    }
 
     /**
      * @constructor
      */
 
-    function Snowflake(){
+    function Snowflake(y){
 
+        let index = Math.random() * flakes.length | 0;
+        let flake = flakes[index];
+
+        this.buffer = buffer[index] || createBuffer(index);
+        this.r = flake.r * dpi * quality;
+        this.w = Math.ceil(flake.r * dpi * quality * size);
         this.x = randomize(width);
-        this.y = randomize(height * -1);
-        this.r = 0.5 + randomize(3);
-        this.o = 0.3 + randomize(0.7);
-        this.vy = 1 + randomize(3);
-        this.vx = 0.5 - randomize();
+        this.y = y || -this.w;
+        this.o = flake.o;
+        this.vy = flake.r * flake.r * dpi * quality / size / 2;
+        this.vx = (0.5 - randomize()) / size * dpi * quality;
+        this.deg = (1 - randomize(2)) * size / 180 * Math.PI;
     }
 
-    function init(){
+    function update(time){
 
-        snowflakes = new Array(count);
-
-        for(var i = 0; i < count; i++) {
-
-            snowflakes[i] = new Snowflake();
-        }
-    }
-
-    function update(){
-
-        window.requestAnimationFrame(update);
-
+        raf = window.requestAnimationFrame(update);
+        if(!image.loaded) return;
         ctx.clearRect(0, 0, width, height);
 
-        for(var i = 0; i < count; i++) {
+        let scale = last ? (time - last) / (1000 / 60) : 1;
+        last = time;
 
-            snowflake = snowflakes[i];
-            snowflake.y += snowflake.vy;
-            snowflake.x += snowflake.vx;
+        for(let i = 0, flake; i < (count * density | 0); i++) {
 
-            if((snowflake.y > height) ||
-               (snowflake.x > width) ||
-               (snowflake.x < 0)){
+            flake = snowflakes[i] || (snowflakes[i] = new Snowflake(randomize(height)));
+            flake.y += flake.vy / scale * speed * (size / 3);
+            flake.x += flake.vx / scale + Math.sin(flake.y / speed / (size / 3) * flake.deg) / 2;
 
-                snowflakes[i] = new Snowflake();
+            if(flake.y >= height ||
+               flake.x >= width ||
+               flake.x <= -flake.w){
+
+                snowflakes.length > (count * density | 0)
+                    ? snowflakes.splice(i--, 1)
+                    : snowflakes[i] = new Snowflake(0);
             }
-            else if(snowflake.y > 0){
+            else{
 
-                ctx.globalAlpha = snowflake.o;
-                ctx.beginPath();
-                ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
-                ctx.closePath();
-                ctx.fill();
+                ctx.drawImage(
+                    flake.buffer,
+                    0,
+                    0,
+                    flake.w,
+                    flake.w,
+                    flake.x,
+                    flake.y,
+                    flake.w,
+                    flake.w
+                );
             }
         }
     }
 
     function resize(){
 
-        var element = document.documentElement,
+        let element = document.documentElement,
             body = document.body;
 
-        width = window.innerWidth || element.clientWidth || body.clientWidth;
-        height = window.innerHeight || element.clientHeight || body.clientHeight;
-        count = (width * height / 7500) >> 0;
+        width = (window.innerWidth || element.clientWidth || body.clientWidth) / 3 * dpi * quality;
+        height = (window.innerHeight || element.clientHeight || body.clientHeight) / 3 * dpi * quality;
+        count = ((width / (dpi * quality)) * (height / (dpi * quality)) / 1500) | 0;
 
         canvas.width = width;
         canvas.height = height;
-        ctx.fillStyle = "#FFF";
-
-        init();
     }
 
     /**
@@ -95,30 +155,109 @@
 
     function randomize(bound){
 
-        return Math.random() * (bound || 1)
+        return Math.random() * (bound || 1);
+    }
+
+    function style(styles){
+
+        const style = canvas.style;
+
+        for(const key in styles){
+
+            style.setProperty(key, styles[key]);
+        }
     }
 
     function load(){
 
         resize();
-        update();
+        update(0);
 
-        document.body.appendChild(canvas);
+        autostart = true;
+        canvas.setAttribute("id", "snowflake");
+        canvas.parentElement || document.body.appendChild(canvas);
     }
 
-    function addEvent(element, eventName, fn) {
+    window.addEventListener("resize", resize, false);
 
-        if(element.addEventListener){
-
-            element.addEventListener(eventName, fn, false);
+    const controls = window["Snowflake"] = {
+        "style": style,
+        "speed": function(val){
+            speed = val;
+        },
+        "size": function(val){
+            size = 3 * val;
+            buffer.length = 0;
+        },
+        "quality": function(val){
+            quality = 3 / 2 * Math.min(Math.max(val, 0.1), 2);
+            buffer.length = 0;
+            snowflakes.length = 0;
+            resize();
+        },
+        "density": function(val){
+            density = val;
+        },
+        "opacity": function(val){
+            style({ "opacity": val });
+        },
+        "index": function(val){
+            style({ "z-index": val });
+        },
+        "image": function(src){
+            image.loaded = !src;
+            src && (image.src = src);
+            buffer.length = 0;
+        },
+        "mount": function(node){
+            node.appendChild(canvas);
+        },
+        "start": function(){
+            autostart
+                ? raf || update(0)
+                : load();
+            style({ "display": "" });
+        },
+        "stop": function(){
+            cancelAnimationFrame(raf);
+            style({ "display": "none" });
+            raf = 0;
         }
-        else{
+    };
 
-            element.attachEvent("on" + eventName, fn);
+    style({
+        "position": "fixed",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "z-index": "999999",
+        "pointer-events": "none"
+    });
+
+    image.loaded = false;
+    image.onload = function(){ image.loaded = true; };
+    image.onerror = function(){ image.onerror = null; image.src = base64; };
+
+    const config = window["SnowflakeConfig"];
+
+    if(config){
+
+        for(let key in config){
+
+            key === "start"
+                ? autostart = config[key] !== false
+                : controls[key](config[key]);
         }
     }
 
-    addEvent(window, "load", load);
-    addEvent(window, "resize", resize);
+    image.loaded || image.src || (image.src = base64);
 
-})(window, document);
+    if(autostart){
+
+        document.readyState === "complete"
+            ? load()
+            : window.addEventListener("load", load, false);
+    }
+
+}());
